8d2265df33155f06b5bbf3723ad34f7bd23df8f2
itsamanthakur <rajputamanthakur12@gmail.com>
2025-11-08 19:20:05 +0530

    add hello

---
+++ src/app/MyComponents/todos-item/todos-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todos-item',
+  imports: [],
+  templateUrl: './todos-item.component.html',
+  styleUrl: './todos-item.component.css'
+})
+export class TodosItemComponent {
+
+}
